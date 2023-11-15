92be9284e1019ddbb53d6d3ac4706b562e11ed96
Hedi-Ayari <hediayari97@gmail.com>
2023-11-15 20:15:36 +0100

    first commit

---
+++ app/src/main/java/com/example/recycleview/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.recycleview;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private final LinkedList<String> mWordList = new LinkedList<>();
+    private RecyclerView mRecyclerView;
+    private WordListAdapter mAdapter;
+    private int mCount = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recyclerview);
+        mAdapter = new WordListAdapter(this, mWordList);
+        mRecyclerView.setAdapter(mAdapter);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        for (int i = 0; i < 20; i++) {
+            mWordList.addLast("Word " + mCount++);
+            Log.d("WordList", mWordList.getLast());
+        }
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                int wordListSize = mWordList.size();
+// Add a new word to the end of the wordList.
+                mWordList.addLast("+ Word " + wordListSize);
+// Notify the adapter, that the data has changed so it can
+// update the RecyclerView to display the data.
+                mRecyclerView.getAdapter().notifyItemInserted(wordListSize);
+// Scroll to the bottom.
+                mRecyclerView.smoothScrollToPosition(wordListSize);
+            }
+        });
+    }
+
+}
